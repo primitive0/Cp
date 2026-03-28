dc5be3111f043e44e119e0d4c71ef0405bfc7db9
primitive0 <94762195+primitive0@users.noreply.github.com>
2026-03-28 19:05:35 +0300

    Добавил дополнительные тесты для SourceBuffer

--- src/cprime/source/source_buffer_unittest.cpp
+++ src/cprime/source/source_buffer_unittest.cpp
@@ -9,6 +9,20 @@ namespace {
 
 using cprime::support::TempFile;
 
+TEST_CASE("SourceBuffer is created from empty file", "[cprime][source]")
+{
+    TempFile source_file{};
+
+    std::unique_ptr<SourceBuffer> buffer =
+        SourceBuffer::from_file(source_file.path());
+
+    REQUIRE(buffer->path() == source_file.path());
+    REQUIRE(buffer->content().empty());
+    REQUIRE(buffer->cbegin() == buffer->begin());
+    REQUIRE(buffer->cend() == buffer->end());
+    REQUIRE(buffer->begin() == buffer->end());
+}
+
 TEST_CASE("SourceBuffer is created from file", "[cprime][source]")
 {
     constexpr std::string_view kSourceText = "fn I32 main() { return 0; }\n";
@@ -23,6 +37,18 @@ TEST_CASE("SourceBuffer is created from file", "[cprime][source]")
     REQUIRE(buffer->cbegin() == buffer->begin());
     REQUIRE(buffer->cend() == buffer->end());
     REQUIRE(std::string_view{buffer->begin(), buffer->end()} == kSourceText);
+}
+
+TEST_CASE("SourceBuffer is created from empty content", "[cprime][source]")
+{
+    std::unique_ptr<SourceBuffer> buffer =
+        SourceBuffer::from_content("");
+
+    REQUIRE(buffer->path().empty());
+    REQUIRE(buffer->content().empty());
+    REQUIRE(buffer->cbegin() == buffer->begin());
+    REQUIRE(buffer->cend() == buffer->end());
+    REQUIRE(buffer->begin() == buffer->end());
 }
 
 TEST_CASE("SourceBuffer is created from content", "[cprime][source]")
@@ -70,20 +96,41 @@ TEST_CASE("SourceSpan is constructed", "[cprime][source]")
     std::unique_ptr<SourceBuffer> buffer =
         SourceBuffer::from_content(std::string{kSourceText});
 
-    // ...U8 |fizz|()...
-    SourceSpan span{
-        *buffer,
-        LineColumn{1, 7},
-        buffer->content().begin() + 6,
-        buffer->content().begin() + 10};
+    SECTION("Empty")
+    {
+        SourceSpan span{
+            *buffer,
+            LineColumn{1, 1},
+            buffer->begin(),
+            buffer->begin()};
 
-    REQUIRE(&span.buffer() == &*buffer);
-    REQUIRE(span.location().line == 1);
-    REQUIRE(span.location().column == 7);
-    REQUIRE(span.content() == "fizz");
-    REQUIRE(span.cbegin() == span.begin());
-    REQUIRE(span.cend() == span.end());
-    REQUIRE(std::string_view{span.begin(), span.end()} == "fizz");
+        REQUIRE(&span.buffer() == &*buffer);
+        REQUIRE(span.location().line == 1);
+        REQUIRE(span.location().column == 1);
+        REQUIRE(span.size() == 0);
+        REQUIRE(span.cbegin() == span.begin());
+        REQUIRE(span.cend() == span.end());
+        REQUIRE(span.begin() == span.end());
+        REQUIRE(span.content().empty());
+    }
+
+    SECTION("With some content")
+    {
+        // ...U8 |fizz|()...
+        SourceSpan span{
+            *buffer,
+            LineColumn{1, 7},
+            buffer->begin() + 6,
+            buffer->begin() + 10};
+
+        REQUIRE(&span.buffer() == &*buffer);
+        REQUIRE(span.location().line == 1);
+        REQUIRE(span.location().column == 7);
+        REQUIRE(span.cbegin() == span.begin());
+        REQUIRE(span.cend() == span.end());
+        REQUIRE(std::string_view{span.begin(), span.end()} == "fizz");
+        REQUIRE(span.content() == "fizz");
+    }
 }
 
 } // namespace
